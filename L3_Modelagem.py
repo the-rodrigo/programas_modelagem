96b1c8c194c01d0de7c9d555459c595df2441bba
Rodrigo Oliveira <oliveira_rodrigo@id.uff.br>
2023-08-08 19:41:49 -0300

    uptading l3

--- L3_Modelagem.py
+++ L3_Modelagem.py
@@ -67,8 +67,8 @@ def set_NR(tolerance, T0):
         i += 1
     return T[-1], C[-1]
 
+
 # Parâmetros
-'''
 
 q: float = 0.1
 v: float = 0.1
@@ -104,7 +104,7 @@ T_estacionario.append(Tee)
 C_estacionario.append(Cee)
 
 
-def dSdt(S, t):
+def CSTR(S, t):
     C, T = S
     dCdt = q * Cf / v - C * (k0 * math.exp(-E / (R * T)) + q / v)
     dTdt = q * Tf / v + deltaH_neg * k0 * C * \
@@ -113,9 +113,9 @@ def dSdt(S, t):
     return [dCdt, dTdt]
 
 
-def plot_trajetories(t, T0, C0, line_type='k-', label=None):
+def plot_CSTR_trajetories(t, T0, C0, line_type='k-', label=None):
     S0 = [C0, T0]
-    sol = odeint(dSdt, S0, t)
+    sol = odeint(CSTR, S0, t)
 
     C_plot = sol[:, 0]
     T_plot = sol[:, 1]
@@ -123,27 +123,31 @@ def plot_trajetories(t, T0, C0, line_type='k-', label=None):
     plt.plot(C_plot, T_plot, line_type, lw=1, label=label)
 
 
-def plot_separatrix(t, inf, Tee0, Cee0):
+def plot_CSTR_separatrix(t, inf, Tee0, Cee0):
     T0 = Tee0 + inf
     C0 = Cee0
-    plot_trajetories(t, T0, C0, 'k--', 'Separatriz')
+    plot_CSTR_trajetories(t, T0, C0, 'k--', 'Separatriz')
 
     T0 = Tee0 - inf
     C0 = Cee0
-    plot_trajetories(t, T0, C0, 'k--', 'Separatriz')
+    plot_CSTR_trajetories(t, T0, C0, 'k--', 'Separatriz')
 
 
 t = np.linspace(0, -10, 1000)
 inf = 0.01
-plot_separatrix(t, inf, T_estacionario[0], C_estacionario[0])
+plot_CSTR_separatrix(t, inf, T_estacionario[0], C_estacionario[0])
 
 t = np.linspace(0, 20, 1000)
 T_initial = [330, 360]
 C_initial = [9, 1]
 
-plot_trajetories(t, T_initial[0], C_initial[0], 'b-', 'Simulação Dinâmica')
+plot_CSTR_trajetories(
+    t, T_initial[0], C_initial[0], 'b-', 'Simulação Dinâmica'
+)
 
-plot_trajetories(t, T_initial[1], C_initial[1], 'g-', 'Simulação Dinâmica')
+plot_CSTR_trajetories(
+    t, T_initial[1], C_initial[1], 'g-', 'Simulação Dinâmica'
+)
 
 plt.scatter(C_estacionario, T_estacionario, color='r',
             s=15, label='Estados Estacionários')
@@ -160,7 +164,7 @@ plt.show()
 
 # Letra B
 
-n = 10000
+n = 9000
 Tcf_vector = np.linspace(300, 400, n)
 Real_1 = np.ones(n)
 Imag_1 = np.ones(n)
@@ -217,117 +221,187 @@ plt.plot(Real_2[0], Tcf_vector[0], 'bx', Real_2[n-1],
          Tcf_vector[n-1], 'bo', linewidth=1, markersize=5)
 plt.legend(['λ_1', 'λ_2', 'Begin', 'End', 'Begin', 'End'])
 plt.show()
-'''
+
+# Número 4
+
+
+def VdP(X, t):
+    x1, x2 = X
+    dx1_dt = x2
+    dx2_dt = mi*(1 - x1**2)*x2 - x1
+    return [dx1_dt, dx2_dt]
+
+
+def plot_trajetories_VdP(X0_1, X0_2, t, mi):
+    mi = mi
+    if mi < 0:
+        sol = odeint(VdP, X0_1, t)
+        x1_plot = sol[:, 0]
+        x2_plot = sol[:, 1]
+    elif mi == 0:
+        X0 = [0.001, 0]
+        sol = odeint(VdP, X0, t)
+        x1_plot = sol[:, 0]
+        x2_plot = sol[:, 1]
+    else:
+        sol = odeint(VdP, X0_2, t)
+        x1_plot = sol[:, 0]
+        x2_plot = sol[:, 1]
+    return x1_plot, x2_plot
+
+
+mi_values = [-3, -2, -1, 0, 1, 2, 3]
+t = np.linspace(0, 20, 1000)
+X0_1 = [-1, 1]
+X0_2 = [0.001, 0]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for mi in mi_values:
+    x1_plot, x2_plot = plot_trajetories_VdP(X0_1, X0_2, t, mi)
+
+    ax.plot(mi * np.ones(len(x1_plot)), x1_plot, x2_plot)
+
+X0_1 = [1, -1]
+X0_2 = [2, -2]
+
+for mi in mi_values:
+    x1_plot, x2_plot = plot_trajetories_VdP(X0_1, X0_2, t, mi)
+
+    ax.plot(mi * np.ones(len(x1_plot)), x1_plot, x2_plot)
+
+ax.set_xlabel('μ')
+ax.set_ylabel('x1')
+ax.set_zlabel('x2')
+ax.set_title('Plano de fases variando μ')
+ax.set_xlim(-3, 3)
+ax.set_ylim(-5, 5)
+ax.set_zlim(-5, 5)
+plt.show()
+
 # Número 5
 
-# def dPdt(P,t):
-#     ro, teta = P
-#     dro_dt = 1 - ro**2
-#     dteta_dt = 1
-#     return [dro_dt, dteta_dt]
+
+def Stable_Orbit(P, t):
+    ro, teta = P
+    dro_dt = 1 - ro**2
+    dteta_dt = 1
+    return [dro_dt, dteta_dt]
 
 
-# def limit_cicle(P0,t):
-#     X0 = [P0[0]*np.cos(P0[1]),P0[0]*np.sin(P0[1])]
-#     sol = odeint(dPdt, P0, t)
+def limit_cicle_stable(P0, t):
+    X0 = [P0[0]*np.cos(P0[1]), P0[0]*np.sin(P0[1])]
+    sol = odeint(Stable_Orbit, P0, t)
 
-#     ro_plot = sol[:, 0]
-#     teta_plot = sol[:, 1]
-#     x1 = []
-#     x2 = []
+    ro_plot = sol[:, 0]
+    teta_plot = sol[:, 1]
+    x1 = []
+    x2 = []
 
-#     for i in range(len(ro_plot)):
-#         x1.append(ro_plot[i]*np.cos(teta_plot[i]))
-#         x2.append(ro_plot[i]*np.sin(teta_plot[i]))
+    for i in range(len(ro_plot)):
+        x1.append(ro_plot[i]*np.cos(teta_plot[i]))
+        x2.append(ro_plot[i]*np.sin(teta_plot[i]))
 
-#     plt.plot(x1, x2, lw=1)
-#     return x1[-1], x2[-1], X0
+    plt.plot(x1, x2, lw=1)
+    return x1[-1], x2[-1], X0
 
-# x1_points = []
-# x2_points = []
-# x0_points1 = []
-# x0_points2 = []
 
-# t = np.linspace(0, 20, 1000)
-# P0 = [np.sqrt(2),np.pi/4]
-# x1_value, x2_value, x0_value = limit_cicle(P0, t)
-# x1_points.append(x1_value)
-# x2_points.append(x2_value)
-# x0_points1.append(x0_value[0])
-# x0_points2.append(x0_value[1])
+x1_points = []
+x2_points = []
+x0_points_x1 = []
+x0_points_x2 = []
 
-# P0 = [0,0]
-# x1_value, x2_value, x0_value = limit_cicle(P0, t)
-# x1_points.append(x1_value)
-# x2_points.append(x2_value)
-# x0_points1.append(x0_value[0])
-# x0_points2.append(x0_value[1])
+t = np.linspace(0, 20, 1000)
+P0 = [1.5, 0]
+x1_value, x2_value, x0_value = limit_cicle_stable(P0, t)
+x1_points.append(x1_value)
+x2_points.append(x2_value)
+x0_points_x1.append(x0_value[0])
+x0_points_x2.append(x0_value[1])
 
-# plt.scatter(x1_points,x2_points, color='r',s=15, label='EE')
-# plt.scatter(x0_points1,x0_points2, color='c', s=35,
-#                 marker='x', label = 'CI')  # type: ignore
-# plt.legend(loc = 'upper left')
-# plt.show()
+P0 = [0, 0]
+x1_value, x2_value, x0_value = limit_cicle_stable(P0, t)
+x1_points.append(x1_value)
+x2_points.append(x2_value)
+x0_points_x1.append(x0_value[0])
+x0_points_x2.append(x0_value[1])
+
+plt.scatter(x1_points, x2_points, color='r', s=15, label='EE')
+plt.scatter(x0_points_x1, x0_points_x2, color='c', s=35,
+            marker='x', label='CI')  # type: ignore
+plt.title('Plano de Fases com Ciclo Limite Estável')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.legend(loc='upper left')
+plt.show()
 
 # Número 6
 
-# def dUdt(P,t):
-#     ro, teta = P
-#     dro_dt = ro*(ro**2 - 1)
-#     dteta_dt = 1
-#     return [dro_dt, dteta_dt]
+
+def Unstable_Orbit(P, t):
+    ro, teta = P
+    dro_dt = ro*(ro**2 - 1)
+    dteta_dt = 1
+    return [dro_dt, dteta_dt]
 
 
-# def limit_cicle(P0,t):
-#     X0 = [P0[0]*np.cos(P0[1]),P0[0]*np.sin(P0[1])]
-#     sol = odeint(dUdt, P0, t)
+def limit_cicle_unstable(P0, t):
+    X0 = [P0[0]*np.cos(P0[1]), P0[0]*np.sin(P0[1])]
+    sol = odeint(Unstable_Orbit, P0, t)
 
-#     ro_plot = sol[:, 0]
-#     teta_plot = sol[:, 1]
-#     x1 = []
-#     x2 = []
+    ro_plot = sol[:, 0]
+    teta_plot = sol[:, 1]
+    x1 = []
+    x2 = []
 
-#     for i in range(len(ro_plot)):
-#         x1.append(ro_plot[i]*np.cos(teta_plot[i]))
-#         x2.append(ro_plot[i]*np.sin(teta_plot[i]))
+    for i in range(len(ro_plot)):
+        x1.append(ro_plot[i]*np.cos(teta_plot[i]))
+        x2.append(ro_plot[i]*np.sin(teta_plot[i]))
 
-#     plt.plot(x1, x2, lw=1)
-#     return x1[-1], x2[-1], X0
+    plt.plot(x1, x2, lw=1)
+    return x1[-1], x2[-1], X0
 
-# x1_points = []
-# x2_points = []
-# x0_points1 = []
-# x0_points2 = []
 
-# t = np.linspace(0, 20, 1000)
-# P0 = [0.5,0]
-# x1_value, x2_value, x0_value = limit_cicle(P0, t)
-# x1_points.append(x1_value)
-# x2_points.append(x2_value)
-# x0_points1.append(x0_value[0])
-# x0_points2.append(x0_value[1])
+x1_points = []
+x2_points = []
+x0_points_x1 = []
+x0_points_x2 = []
 
-# t = np.linspace(0,-10,1000)
-# P0 = [1.5,0]
-# x1_value, x2_value, x0_value = limit_cicle(P0, t)
-# x1_points.append(x1_value)
-# x2_points.append(x2_value)
-# x0_points1.append(x0_value[0])
-# x0_points2.append(x0_value[1])
+t = np.linspace(0, 20, 1000)
+P0 = [0.5, 0]
+x1_value, x2_value, x0_value = limit_cicle_unstable(P0, t)
+x1_points.append(x1_value)
+x2_points.append(x2_value)
+x0_points_x1.append(x0_value[0])
+x0_points_x2.append(x0_value[1])
 
-# plt.scatter(x1_points,x2_points, color='r',s=15, label='EE')
-# plt.scatter(x0_points1,x0_points2, color='c', s=35,
-#                 marker='x', label = 'CI')  # type: ignore
-# plt.legend(loc = 'upper left')
-# plt.show()
+t = np.linspace(0, -10, 1000)
+P0 = [1.5, 0]
+x1_value, x2_value, x0_value = limit_cicle_unstable(P0, t)
+x1_points.append(x1_value)
+x2_points.append(x2_value)
+x0_points_x1.append(x0_value[0])
+x0_points_x2.append(x0_value[1])
+
+plt.scatter(x1_points, x2_points, color='r', s=15, label='EE')
+plt.scatter(x0_points_x1, x0_points_x2, color='c', s=35,
+            marker='x', label='CI')  # type: ignore
+plt.title('Plano de Fases com Ciclo Limite Instável')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.legend(loc='upper left')
+plt.show()
 
 # Número 7
 
-def Duffing(X,t):
+
+def Duffing(X, t):
     x1, x2 = X
     dx1_dt = x2
     dx2_dt = - x2 + x1 - x1**3
     return [dx1_dt, dx2_dt]
+
 
 def plot_Duffing_trajetories(t, x1_init, x2_init, line_type='k-', label=None):
     X0 = [x1_init, x2_init]
@@ -354,19 +428,23 @@ inf = 0.01
 plot_Duffing_separatrix(t, inf, 0, 0)
 
 t = np.linspace(0, 20, 1000)
-x1_initial = [5,-5]
+x1_initial = [5, -5]
 x2_initial = [1, -1]
 
-plot_Duffing_trajetories(t, x1_initial[0], x2_initial[0], 'b-', 'Simulação Dinâmica')
+plot_Duffing_trajetories(
+    t, x1_initial[0], x2_initial[0], 'b-', 'Simulação Dinâmica')
 
-plot_Duffing_trajetories(t, x1_initial[1], x2_initial[1], 'g-', 'Simulação Dinâmica')
+plot_Duffing_trajetories(
+    t, x1_initial[1], x2_initial[1], 'g-', 'Simulação Dinâmica')
 
-x1_estacionario = [0,1,-1]
-x2_estacionario = [0,0,0]
+x1_estacionario = [0, 1, -1]
+x2_estacionario = [0, 0, 0]
 plt.scatter(x1_estacionario, x2_estacionario, color='r',
             s=15, label='Estados Estacionários')
 plt.scatter(x1_initial, x2_initial, color='c', s=15,
             marker='x', label='Condição Inicial')  # type: ignore
-
+plt.title('Plano de Fases - Oscilador de Duffing')
+plt.xlabel('x1')
+plt.ylabel('x2')
 plt.legend()
 plt.show()
